761bb4702d19a552b97d74db720855f322085c22
Florian Kesseler <florian.kesseler@alliedvision.com>
2021-08-26 12:39:35 +0000

    Updated Application name

--- Source/Source/V4L2Viewer.cpp
+++ Source/Source/V4L2Viewer.cpp
@@ -43,7 +43,7 @@
 #define BIT_DEPTH 8
 
 #define MANUF_NAME_AV       "Allied Vision"
-#define APP_NAME            "Video4Linux2 Viewer"
+#define APP_NAME            "Allied Vision V4L2 Viewer"
 #define APP_VERSION_MAJOR   1
 #define APP_VERSION_MINOR   0
 #define APP_VERSION_PATCH   0
