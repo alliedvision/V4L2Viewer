cef9075bec71e3e217ffbfa82b78ee3d0c321a62
Florian Keßeler <florian.kesseler@alliedvision.com>
2022-05-09 13:16:43 +0200

    Set length of int64 fields in 'all features' dialog to necessary number

--- Source/Source/EnumeratorInterface/Integer64EnumerationControl.cpp
+++ Source/Source/EnumeratorInterface/Integer64EnumerationControl.cpp
@@ -39,6 +39,9 @@ Integer64EnumerationControl::Integer64EnumerationControl(int32_t id, int64_t min
     m_MainLayout.addWidget(&m_Slider, 0, 1);
     m_MainLayout.addWidget(&m_LineEdit, 0, 2);
 
+    auto const len = int(std::ceil(std::log10(max)));
+    m_LineEdit.setMaxLength(len);
+
     m_MainLayout.setColumnStretch(0, 2);
     m_MainLayout.setColumnStretch(1, 2);
     m_MainLayout.setColumnStretch(2, 1);
